6b3df47967babb40991e87a98b978ebc4061284d
Pulkit Singh <pulkitsingh@Pulkits-MacBook-Pro.local>
2021-11-25 10:39:22 +0530

    withchanges

---
+++ src/main/java/com/sygnific/careers/sygnific/careers/SygnificCareersApplication.java
@@ -0,0 +1,14 @@
+package com.sygnific.careers.sygnific.careers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SygnificCareersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SygnificCareersApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/com/sygnific/careers/sygnific/careers/SygnificCareersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sygnific.careers.sygnific.careers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SygnificCareersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
